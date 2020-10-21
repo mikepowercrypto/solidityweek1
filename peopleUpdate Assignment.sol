a377bc516721287396ea863da9f4da7d8a76a16d
mikepowercrypto <71322969+mikepowercrypto@users.noreply.github.com>
2020-10-21 20:21:19 +1100

    Update peopleUpdate Assignment.sol
    Fixed Assignment

--- peopleUpdate Assignment.sol
+++ peopleUpdate Assignment.sol
@@ -10,9 +10,11 @@ contract HelloWorld{
       bool senior;
     }
 
+    event personDeleted(string name, bool senior, address deletedBy);
     
-    event personcreated(string name, uint age);
-    event personUpdated(string name, uint age, uint height, bool senior);
+    event personCreated(string name, uint age);
+    
+    event personUpdated(string name, uint age, uint height);
 
     address public owner;
 
@@ -32,21 +34,33 @@ contract HelloWorld{
     function createPerson(string memory name, uint age, uint height) public {
       require(age < 150, "Age needs to be below 150");
        
-        Person memory newPerson;
-        newPerson.name = name;
-        newPerson.age = age;
-        newPerson.height = height;
+        if (people[msg.sender].age != 0){
+            people[msg.sender].age = age;
+            people[msg.sender].height = height;
+            people[msg.sender].name = name;
+      
+            emit personUpdated(name, age, height);
+          
+        }
+    
+        else {
+          
+            Person memory newPerson;
+            newPerson.name = name;
+            newPerson.age = age;
+            newPerson.height = height;
         
 
-        if(age >= 65){
-           newPerson.senior = true;
-       }
-       else{
-           newPerson.senior = false;
+        if (age >= 65){
+            newPerson.senior = true;
+        }
+        
+        else {
+            newPerson.senior = false;
        }
 
-        insertPerson(newPerson);
-        creators.push(msg.sender);
+            insertPerson(newPerson);
+            creators.push(msg.sender);
 
         assert(
             keccak256(
@@ -67,10 +81,9 @@ contract HelloWorld{
                 )
             )
         );
-        
-        
-        emit personcreated(newPerson.name, newPerson.age);
-        emit personUpdated(newPerson.name, newPerson.age, newPerson.height, newPerson.senior);
+             emit personCreated(newPerson.name, newPerson.age); 
+          
+      }
         
     }
     function insertPerson(Person memory newPerson) private {
@@ -82,7 +95,15 @@ contract HelloWorld{
         return (people[creator].name, people[creator].age, people[creator].height, people[creator].senior);
     }
    
-   function getCreator(uint index) public view onlyOwner returns(address){
+    function deletePerson(address creator) public onlyOwner{
+       string memory name = people[creator].name;
+       bool senior = people[creator].senior;
+       
+       delete people[creator];
+       assert(people[creator].age == 0);
+       emit personDeleted(name, senior, owner);
+   }
+    function getCreator(uint index) public view onlyOwner returns(address){
        return creators[index];
    }
 
